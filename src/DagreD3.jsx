27d9fd6e423c01899de90fec8394f32551b9904b
linchengzhang <lcz_1996@foxmail.com>
2018-11-13 19:11:21 +0800

    fix error when props of edge is undefined

--- src/DagreD3.jsx
+++ src/DagreD3.jsx
@@ -13,6 +13,7 @@ class DagreD3 extends React.Component {
     static defaultProps = {
         height: "1",
         width: "1",
+        graph: {},
         // width and height are defaulted to 1 due to a FireFox bug(?) If set to 0, it complains.
         fit: true,
         interactive: false
@@ -21,6 +22,7 @@ class DagreD3 extends React.Component {
     static propTypes = {
         nodes: PropTypes.object.isRequired,
         edges: PropTypes.array.isRequired,
+        graph: PropTypes.object,
         interactive: PropTypes.bool,
         fit: PropTypes.bool,
         height: PropTypes.string,
@@ -44,14 +46,19 @@ class DagreD3 extends React.Component {
     }
 
     renderDag() {
-        let g = new dagreD3.graphlib.Graph().setGraph({});
+        const { nodes, edges, graph, fit, onNodeClick } = this.props;
 
+        const g = new dagreD3.graphlib.Graph()
+            .setGraph({ ...graph }) // Set an object for the graph label
+            .setDefaultEdgeLabel({}); // Default to assigining a new object as a label for each new edge
 
-        for (let [id, node] of Object.entries(this.props.nodes))
-            g.setNode(id, node);
-
-        for (let edge of this.props.edges)
-            g.setEdge(edge[0], edge[1], edge[2]); // from, to, props
+        Object.keys(nodes).forEach((id) => {
+            g.setNode(id, nodes[id]);
+        })
+        
+        edges.forEach((e) => {
+            e[2] ? g.setEdge(e[0], e[1], e[2]) : g.setEdge(e[0], e[1]) // when props is undefined
+        })
 
         // Set up an SVG group so that we can translate the final graph.
         let svg = d3.select(this.nodeTree);
@@ -68,36 +75,33 @@ class DagreD3 extends React.Component {
         let render = new dagreD3.render();
 
         // set up custom shape renderers
-        if (this.props.shapeRenderers)
+        if (this.props.shapeRenderers) {
             for (let [shape, renderer] of Object.entries(this.props.shapeRenderers))
                 render.shapes()[shape] = renderer;
+        }
 
         // Run the renderer. This is what draws the final graph.
         render(inner, g);
 
-
         // TODO add padding?
-        if (this.props.fit) {
-            let {height: gHeight, width: gWidth} = g.graph();
-            let {height, width} = this.nodeTree.getBBox();
+        if (fit) {
+            let { height: gHeight, width: gWidth } = g.graph();
+            let { height, width } = this.nodeTree.getBBox();
             let transX = width - gWidth;
             let transY = height - gHeight;
-            svg.attr("height", height);
-            svg.attr("width", width);
-            inner.attr("transform", d3.zoomIdentity.translate(transX, transY))
+            svg.attr('viewBox', `0 0 ${width} ${height}`); // adaptation
+            inner.attr("transform", d3.zoomIdentity.translate(transX, transY));
         }
-
-        if (this.props.onNodeClick)
+        
+        if (onNodeClick) {
             svg.selectAll('.dagre-d3 .node').on('click',
-                    id => this.props.onNodeClick(id));
+                    id => onNodeClick(id));
+        }
     }
 
     render() {
         return (
-            <svg className='dagre-d3' ref={(r) => {this.nodeTree = r}}
-                 width={this.props.height}
-                 height={this.props.width}>
-
+            <svg width="100%" height="100%" className='dagre-d3' ref={(r) => {this.nodeTree = r}}>
                 <g ref={(r) => {this.nodeTreeGroup = r}}/>
             </svg>
         );
